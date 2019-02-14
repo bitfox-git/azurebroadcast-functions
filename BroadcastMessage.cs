4fe582c252d8bbab4a5f14859d97dbf4c47a1f30
voslucas <vos.lucas@outlook.com>
2019-02-14 15:54:36 +0100

    Shorter json message fieldnames

--- BroadcastMessage.cs
+++ BroadcastMessage.cs
@@ -1,10 +1,17 @@
 
+using Newtonsoft.Json;
+
 namespace Bitfox.AzureBroadcast.Functions
 {
      public class BroadcastMessage {
+            
+            [JsonProperty(PropertyName = "jm")]
             public string jsonmessage {get;set;}
+            [JsonProperty(PropertyName = "fu")]
             public string fromUser {get;set;}
+            [JsonProperty(PropertyName = "tu")]
             public string toUser {get;set;}
+            [JsonProperty(PropertyName = "gn")]
             public string toGroupName {get;set;}
 
         }
